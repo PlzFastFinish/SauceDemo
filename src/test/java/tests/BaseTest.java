d9c00765404dea04d4949b74a4d08125d17cf795
Fedor <plzfastfinish33@gmail.com>
2025-07-06 18:26:45 +0400

    PO and Locators

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,55 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.asserts.SoftAssert;
+import pages.*;
+
+import java.time.Duration;
+import java.util.HashMap;
+
+public class BaseTest {
+
+    final String LOGIN_STANDARD_USER = "standard_user";
+    final String CORRECT_PASSWORD = "secret_sauce";
+
+    WebDriver driver;
+    SoftAssert softAssert;
+    LoginPage loginPage;
+    ProductsPage productsPage;
+    CartPage cartPage;
+    CheckoutYourInformationPage checkoutYourInformationPage;
+    CheckoutOverviewPage checkoutOverviewPage;
+    CheckoutCompletePage checkoutCompletePage;
+
+    @BeforeMethod
+    public void setup() {
+        ChromeOptions options = new ChromeOptions();
+        HashMap<String, Object> chromePrefs = new HashMap<>();
+        chromePrefs.put("credentials_enable_service", false);
+        chromePrefs.put("profile.password_manager_enabled", false);
+        options.setExperimentalOption("prefs", chromePrefs);
+        options.addArguments("--incognito");
+        options.addArguments("--disable-notifications");
+        options.addArguments("--disable-popup-blocking");
+        options.addArguments("--disable-infobars");
+        options.addArguments("--start-maximized");
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        softAssert = new SoftAssert();
+        loginPage = new LoginPage(driver);
+        productsPage = new ProductsPage(driver);
+        cartPage = new CartPage(driver);
+        checkoutYourInformationPage = new CheckoutYourInformationPage(driver);
+        checkoutOverviewPage = new CheckoutOverviewPage(driver);
+        checkoutCompletePage = new CheckoutCompletePage(driver);
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void close() {
+        driver.quit();
+    }
+}
---
+++ src/test/java/tests/ShoppingCartTest.java
@@ -0,0 +1,23 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class ShoppingCartTest extends BaseTest {
+
+    @Test
+    public void checkItemNameAndPrice() {
+        loginPage.open();
+        loginPage.login(LOGIN_STANDARD_USER, CORRECT_PASSWORD);
+        productsPage.addItemToCart();
+        String itemName = productsPage.getItemNameOnProductsPage();
+        String itemPrice = productsPage.getItemPrice();
+        productsPage.moveToCart();
+        softAssert.assertEquals(itemName,
+                cartPage.getItemName(),
+                "Название не соответствует");
+        softAssert.assertEquals(itemPrice,
+                cartPage.getItemPrice(),
+                "Цена не соответствует");
+        softAssert.assertAll();
+    }
+}
